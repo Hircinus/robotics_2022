83b1ea56715c2ec82b29f843ab349288440db7f7
Hircinus <j.alfahad@gmail.com>
2022-03-28 22:16:39 -0400

    add elessia's img

---
+++ fr/a_propos.php
@@ -0,0 +1,117 @@
+<?php
+$pageEn = 'about';
+$pageTitle = '&Agrave; Propos';
+$active = 1;
+require '../layouts/header.php'; ?>
+
+<section class="hero is-info is-medium has-text-left">
+  <div class="hero-body">
+    <div class="content">
+      <h1 class="title">
+        CRC Robotique 2022
+      </h1>
+    </div>
+  </div>
+  <div class="hero-foot">
+    <nav class="tabs is-boxed is-fullwidth">
+      <div class="container">
+        <ul>
+          <li class="is-active">
+            <a class="tab-btn" data-target="0">What we're all about</a>
+          </li>
+          <li>
+            <a class="tab-btn" data-target="1">Sponsors</a>
+          </li>
+          <li>
+            <a class="tab-btn" data-target="2">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</section>
+
+<main class="content-area fadeInUp show">
+  <div class="columns">
+    <div class="column is-full">
+      <div class="content">
+        <h1>What we're all about</h1>
+        <hr>
+      </div>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column is-three-quarters">
+      <div class="content">
+        <p>
+        L'équipe Robotique de Champlain est plus qu'une simple équipe d'ingénieurs. En plus de la conception et de la construction d'un robot, nous et les autres participants sont jugés sur :
+        </p>
+        <ul>
+          <li>La construction d'un kiosque.</li>
+          <li>La résolution d'une compétition de programmation.</li>
+          <li>La conception et le développement d'un site Web.</li>
+          <li>La production de deux vidéos : une pour présenter notre équipe et une autre pour fournir un tutoriel.</li>
+      </div>
+    </div>
+    <div class="column is-one-quarter">
+      <div class="content">
+        <img width="auto" height="auto" alt="an image" src="http://www.crhsrobotics.com/2019/assets/team_2014.jpg">
+      </div>
+    </div>
+  </div>
+</main>
+<main class="content-area fadeInUp">
+  <div class="columns">
+    <div class="column is-full">
+      <div class="content">
+        <h1>Sponsors</h1>
+        <hr>
+      </div>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column is-three-quarters">
+      <div class="content">
+        <h3>
+          Our past team (not actually though).
+        </h3>
+        <p>Hello there</p>
+      </div>
+    </div>
+    <div class="column is-one-quarter">
+      <div class="content">
+        <img width="auto" height="auto" alt="an image" src="http://www.crhsrobotics.com/2019/assets/team_2014.jpg">
+      </div>
+    </div>
+  </div>
+</main>
+<main class="content-area fadeInUp">
+  <div class="columns">
+    <div class="column is-full">
+      <div class="content">
+        <h1>Contact</h1>
+        <hr>
+      </div>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column is-three-quarters">
+      <div class="content">
+        <h3>
+          Our past team (not actually though).
+        </h3>
+        <p>Hello there</p>
+      </div>
+    </div>
+    <div class="column is-one-quarter">
+      <div class="content">
+        <img width="auto" height="auto" alt="an image" src="http://www.crhsrobotics.com/2019/assets/team_2014.jpg">
+      </div>
+    </div>
+  </div>
+</main>
+
+
+<?php
+require '../layouts/footer.php';
+?>
